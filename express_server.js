fc2a208188e9b9e3188415e9a9817f45de199d7f
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-11-09 19:11:52 -0500

    Add POST and another GET route for new template, implement a temporary function

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app =  express();
 const PORT = 8080;
 
+app.use(express.urlencoded({ extended: true })); // Translate 'Buffer' data type into a string we can read
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -31,7 +33,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new"); // Use urls_new Template for this route
+});
+
+app.post("/urls", (req, res) => {
+  console.log("shortURL:", generateRandomString()) // generates random 6 characters (temporary)
+  console.log(req.body); // returns longURL as a JS object
+  res.send("ok"); // Server response "Ok" (temporary)
+  })
+  
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase };
   res.render("urls_show", templateVars);
-});
+  });
+    
+function generateRandomString() {
+  return Math.random().toString(36).slice(2, 8); // Temporary shortURL generator
+};
