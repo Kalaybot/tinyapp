3424174de37ef3f2788f025aae1dc953741711e3
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-11-09 22:27:44 -0500

    Store short and long URL value pair, redirect /urls to /urls/:id

--- express_server.js
+++ express_server.js
@@ -21,33 +21,52 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls.json", (req, res) => { // Route setup for /urls.json
-  res.json(urlDatabase); // http://localhost:8080/urls.json response as a JSON string of the entire urlDatabase
+  res.json(urlDatabase); // Server response a JSON string of entire database
 });
 
 app.get("/hello", (req, res) => { // Route setup for /hello
-  res.send("<html><body>Hello <b>World</b></body></html>\n"); // When visiting http://localhost:8080/hello it would show "Hello World"
+  res.send("<html><body>Hello <b>World</b></body></html>\n"); // server route says "Hello World"
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }; // Route setup for /urls
+  const templateVars = { urls: urlDatabase }; // Route setup for saved urls
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new"); // Use urls_new Template for this route
+  res.render("urls_new"); // Direct to a route with a form to complete
 });
 
 app.post("/urls", (req, res) => {
-  console.log("shortURL:", generateRandomString()) // generates random 6 characters (temporary)
   console.log(req.body); // returns longURL as a JS object
-  res.send("ok"); // Server response "Ok" (temporary)
-  })
+
+  const longURL = req.body.longURL; // Extract longURL to the body
+  const id = generateRandomString(); // Generates the new id
+
+  urlDatabase[id] = longURL;
+
+  res.redirect(`/urls/${id}`); // Redirect to urls/:id. id has value now
+});
   
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase };
+  const templateVars = { id: req.params.id, longURL: urlDatabase }; // newly created URLs
   res.render("urls_show", templateVars);
-  });
+});
+
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+
+  res.redirect(longURL);
+});
     
-function generateRandomString() {
-  return Math.random().toString(36).slice(2, 8); // Temporary shortURL generator
-};
+function generateRandomString() { // Function to create the id or shortURL
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+
+  for (let i = 0; i < 6; i++) {
+    const randomCharacters = Math.floor(Math.random() * characters.length);
+    result += characters[randomCharacters];
+  }
+  return result;
+}
